1bfe4698353345a0fa5fa9c287ca2b0246f82cb9
Ezeani Ikenna Evan <32799580+iykeevans@users.noreply.github.com>
2023-09-28 01:40:05 +0100

    implement artists ui

--- src/modules/artists/index.vue
+++ src/modules/artists/index.vue
@@ -1,14 +1,23 @@
 <template>
-  <div>Artists Page</div>
+  <div
+    class="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-x-12 gap-y-12 container mx-auto pt-16 px-5 md:px-0"
+  >
+    <div v-for="artist in artists" :key="artist.id" class="rounded">
+      <artist-card :artist="artist" />
+    </div>
+  </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from "vue";
+<script lang="ts" setup>
+import { onMounted, ref } from "vue";
+import { getArtists } from "./artists.service";
+import { IArtist } from "./artists.typings";
+import ArtistCard from "./components/artist-card.vue";
 
-export default defineComponent({
-  setup() {
-    return {};
-  },
+const artists = ref<IArtist[]>([]);
+
+onMounted(() => {
+  getArtists().then((data) => (artists.value = data));
 });
 </script>
 
--- src/router.ts
+++ src/router.ts
@@ -1,7 +1,14 @@
 import { createRouter, createWebHistory } from "vue-router";
 import artistsRoutes from "./modules/artists/artists.routes";
 
-const routes = [{ path: "/", redirect: "/artists" }, ...artistsRoutes];
+const routes = [
+  {
+    path: "/",
+    component: () => import("./layouts/index.vue"),
+    redirect: "/artists",
+    children: [...artistsRoutes],
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
